881ebf149dafc21542f613780f8def3960c4f41f
jozzee zaadee <jozzeezaadee@gmail.com>
2016-10-28 23:23:36 +0700

    up to version 9.0.2 config notification

--- app/src/main/java/com/sitthiphong/smartgardencare/service/GcmDownstreamService.java
+++ app/src/main/java/com/sitthiphong/smartgardencare/service/GcmDownstreamService.java
@@ -1,6 +1,7 @@
 package com.sitthiphong.smartgardencare.service;
 
 import android.app.ActivityManager;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ComponentName;
@@ -9,7 +10,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -25,26 +28,75 @@ import java.util.List;
 public class GcmDownstreamService extends GcmListenerService {
     private static final String TAG = "DcmDownstreamService";
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onMessageReceived(String from, Bundle data) {
         // TODO Do something here
         Log.e(TAG, "Message Incoming");
-        SharedPreferences sp = getSharedPreferences("Details", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        if (sp.getString("activity", "").equals("onResume")) {
-            Log.e(TAG, "onResume");
-            sendNotification2(String.valueOf(data.get("title")), String.valueOf(data.get("body")));
+        if (isForeground("com.sitthiphong.smartgardencare")) {
+            sendForegroundNotification(getMessage(String.valueOf(data.get("body"))));
 
-        } else if (sp.getString("activity", "").equals("onStop")) {
-            Log.e(TAG, "onResume");
-            sendNotification2(String.valueOf(data.get("title")), String.valueOf(data.get("body")));
-
-        } else if (sp.getString("activity", "").equals("onDestroy")) {
-            Log.e(TAG, "onResume");
-            sendNotification(String.valueOf(data.get("title")), String.valueOf(data.get("body")));
+        } else {
+            sendNotification(
+                    getTitle(String.valueOf(data.get("title"))),
+                    getMessage(String.valueOf(data.get("body"))));
         }
+    }
 
+    /**
+     * @param body 1: water not flows, 2: water not true area, 3: temp not decrease, 7: light in not decrease
+     * @return
+     */
+    private String getMessage(String body) {
+        if (body.equals("1")) {
+            return getResources().getString(R.string.waterFalse);
 
+        } else if (body.equals("2")) {
+            return getResources().getString(R.string.noWateringArea);
+
+        } else if (body.equals("3")) {
+            return getResources().getString(R.string.tempNotDrop);
+
+        } else if (body.equals("7")) {
+            return getResources().getString(R.string.lightNotDrops);
+
+        } else {
+            return body;
+        }
+    }
+
+    private String getTitle(String title) {
+        if (title.equals("except")) {
+            return getResources().getString(R.string.exception);
+        } else {
+            return title;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void sendForegroundNotification(String message) {
+        Intent showTaskIntent = new Intent(this,MainActivity.class);
+        showTaskIntent.setAction(Intent.ACTION_MAIN);
+        showTaskIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        showTaskIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        PendingIntent contentIntent = PendingIntent.getActivities(
+                this,
+                0,
+                new Intent[]{showTaskIntent},
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle(getApplicationContext().getString(R.string.app_name))
+                .setContentText(message)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(contentIntent)
+                .build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notification);
     }
 
     private void sendNotification(String title, String messageBody) {
@@ -84,8 +136,9 @@ public class GcmDownstreamService extends GcmListenerService {
 
         notificationManager.notify(0, notificationBuilder.build());
     }
+
     /**
-     for check activity is running
+     * for check activity is running
      */
     public boolean isForeground(String myPackage) {
         ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
@@ -93,6 +146,7 @@ public class GcmDownstreamService extends GcmListenerService {
         ComponentName componentInfo = runningTaskInfo.get(0).topActivity;
         return componentInfo.getPackageName().equals(myPackage);
     }
+
     public static boolean isBackgroundRunning(Context context) {
         ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
         List<ActivityManager.RunningAppProcessInfo> runningProcesses = am.getRunningAppProcesses();
---
+++ app/src/main/java/com/sitthiphong/smartgardencare/service/NetpieRestAPI.java
@@ -0,0 +1,8 @@
+package com.sitthiphong.smartgardencare.service;
+
+/**
+ * Created by Sitthiphong on 10/16/2016 AD.
+ */
+
+public class NetpieRestAPI {
+}
